fa29cc637ee7c8e577146127a84055fd0137f915
xiangdongdong <xiangdongdong@chelun.com>
2019-10-14 18:07:10 +0800

    点餐

--- src/views/order/menu.vue
+++ src/views/order/menu.vue
@@ -1,22 +1,67 @@
 <template>
-  <el-button type="primary" @click="addGoods">添加</el-button>
+  <div class="menu">
+    <el-row>
+      <el-col :span="8" sty v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0">
+        <el-card :body-style="{ padding: '0px' }">
+          <img
+            src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+            class="image"
+          />
+          <div style="padding: 14px;">
+            <span>好吃的汉堡</span>
+            <div class="bottom clearfix">
+              <time class="time">{{ currentDate }}</time>
+              <el-button type="text" class="button">操作按钮</el-button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
+<style>
+.time {
+  font-size: 13px;
+  color: #999;
+}
+.menu {
+  padding-top: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-bottom: 10px;
+}
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.button {
+  padding: 0;
+  float: right;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both;
+}
+</style>
 
 <script>
-import Detail from "./components/detail";
-
 export default {
-  name: "CreateForm",
-  components: { Detail },
-  methods: {
-    addGoods() {
-      console.log(this.$store.state)
-      var num = this.$store.getters.sidebar.cartNum;
-      num++;
-      this.$store.state.app.sidebar.cartNum=num;
-      
-    }
+  data() {
+    return {
+      currentDate: new Date()
+    };
   }
 };
 </script>
-
